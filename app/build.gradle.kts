3892bd519eb15facbc2a0cb50d247be4c05a37b4
ann_kir <kiritseva@sfedu.ru>
2023-11-06 12:34:06 +0300

    Added carousel-banner, images change each 3 seconds. Added file round-corner shape.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.mitraartapp"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.mitraartapp"
@@ -37,11 +37,16 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.8.0")
+    implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    // Optional: Circle Indicator (To fix the xml preview "Missing classes" error)
+    implementation ("me.relex:circleindicator:2.1.6")
+    implementation("org.imaginativeworld.whynotimagecarousel:whynotimagecarousel:2.1.0")
 }
+
--- app/src/main/java/com/example/mitraartapp/MainActivity.kt
+++ app/src/main/java/com/example/mitraartapp/MainActivity.kt
@@ -1,11 +1,40 @@
 package com.example.mitraartapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.view.View
+import android.view.ViewGroup
+import android.view.animation.AnimationUtils
+import android.widget.ImageButton
+import android.widget.ImageSwitcher
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.ViewSwitcher
+import androidx.appcompat.app.AppCompatActivity
+import me.relex.circleindicator.CircleIndicator2
+import org.imaginativeworld.whynotimagecarousel.ImageCarousel
+import org.imaginativeworld.whynotimagecarousel.model.CarouselItem
+
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
-}
+        val carousel: ImageCarousel = findViewById(R.id.carousel)
+        val list = mutableListOf<CarouselItem>()
+        carousel.setClipToOutline(true)
+
+        list.add(
+            CarouselItem(
+                imageDrawable = R.drawable.c4c4396881322968b97131915f2c7c6d
+            )
+        )
+        list.add(
+            CarouselItem(
+                imageDrawable = R.drawable.f780d31f62d0ef50f3c3363ca7f07117
+            )
+        )
+
+        carousel.addData(list)
+}}
