ef7d12ad969d5d888c2924cb5ff8b89d556f51dc
ann_kir <kiritseva@sfedu.ru>
2023-11-10 11:32:11 +0300

    Finished AccountSettings screen with logic of returning to account overview and home screen.

--- app/src/main/java/com/example/mitraartapp/ui/AccountSettingsActivity.kt
+++ app/src/main/java/com/example/mitraartapp/ui/AccountSettingsActivity.kt
@@ -1,24 +1,27 @@
-package com.example.mitraartapp
+package com.example.mitraartapp.ui
 
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
+import android.widget.ImageButton
 import androidx.fragment.app.Fragment
-import com.example.mitraartapp.ui.AccountSettingsActivity
+import com.example.mitraartapp.AccountFragment
+import com.example.mitraartapp.CartFragment
+import com.example.mitraartapp.HomeFragment
+import com.example.mitraartapp.MainActivity
+import com.example.mitraartapp.MoreFragment
+import com.example.mitraartapp.R
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
-class RegisteredAccountActivity : AppCompatActivity() {
+class AccountSettingsActivity : AppCompatActivity() {
     lateinit var bottomNav : BottomNavigationView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_registered_account)
+        setContentView(R.layout.activity_account_settings)
 
-        // Settings button
-        var buttonSettings = findViewById<Button>(R.id.account_settings_button)
-        buttonSettings.setOnClickListener{
-            val intent = Intent(this@RegisteredAccountActivity, AccountSettingsActivity::class.java)
-            startActivity(intent)
+        var backButton = findViewById<ImageButton>(R.id.back_button)
+        backButton.setOnClickListener{
+            finish()
         }
 
         // Bottom menu
@@ -29,7 +32,7 @@ class RegisteredAccountActivity : AppCompatActivity() {
             when (it.itemId) {
                 R.id.home -> {
                     loadFragment(HomeFragment())
-                    val intent = Intent(this@RegisteredAccountActivity, MainActivity::class.java)
+                    val intent = Intent(this@AccountSettingsActivity, MainActivity::class.java)
                     startActivity(intent)
                     true
                 }
@@ -43,6 +46,7 @@ class RegisteredAccountActivity : AppCompatActivity() {
                 }
                 R.id.account -> {
                     loadFragment(AccountFragment())
+                    finish()
                     true
                 }
 
